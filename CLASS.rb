5c04ca0773fd602aed86a8b5fdc6be629e734725
BaoChung <levanbaochung24022001@gmail.com>
2022-09-19 23:40:21 +0700

    lan2

--- CLASS.rb
+++ CLASS.rb
@@ -1,1 +1,21 @@
-puts 10
+class Sinhvien
+    def initialize
+      puts 'Xin chào bạn!'
+    end      
+    def info
+      print ("nhap diem toan : ")
+      t=gets.chomp.to_f 
+      print ("nhap diem ly : ")
+      l=gets.chomp.to_f
+      print ("nhap diem hoa : ")
+      h=gets.chomp.to_f
+      puts ("điểm toán của sinh viên là : #{t}\nđiểm ly của sinh viên là : #{l} \nđiểm hóa của sinh viên là : #{h}" )
+      tb=((t+l+h)/3).to_f
+      puts ("điểm trung bình của sinh viên là #{tb}")
+    end
+  end
+  sinh_vien = Sinhvien.new
+  puts  sinh_vien.info
+
+  
+  
